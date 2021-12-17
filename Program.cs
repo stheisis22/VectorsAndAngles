740928f6c272cb86f8962990111622994da80713
stheisis22 <44663142+stheisis22@users.noreply.github.com>
2021-12-17 04:31:35 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VectorsAndAngles
+{
+    class Vector
+    {
+        int x;
+        int y;
+        int n = 3;
+
+        public Vector(int X, int Y)
+        {
+            x = X;
+            y = Y;
+        }
+
+        public Vector()
+        {
+
+        }
+
+        public double Magnitude(int X, int Y)
+        {
+            double CalculoMagnitud;
+            CalculoMagnitud = Math.Sqrt((X ^ 2 + Y ^ 2));
+            return CalculoMagnitud;
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethodMagnitudXY()
+        {
+            int x = 1;
+            int y = 2;
+            bool valid = false;
+            double z = Math.Sqrt((x ^ 2 + y ^ 2));
+
+            if (z==2.2360)
+            {
+                Assert.IsTrue(valid);
+            }
+        }
+
+        [TestMethod]
+        public void TesMethodDotProductAB()
+        {
+            int[] vectorA = { 2, 2 };
+            int[] vectorB = { 1, 1 };
+            bool valid = false;
+
+            for (int i = 0; i < 3; i++)
+            {
+                int z = vectorA[i] * vectorB[i];
+
+                if (z==4)
+                {
+                    Assert.IsTrue(valid);
+                }
+            }  
+        }
+
+        [TestMethod]
+        public void TestMethodAngleBetweenAB()
+        {
+            int[] vectorA = { 2, 2 };
+            int[] vectorB = { 1, 1 };
+            double toppart = 0;
+            bool valid = false;
+            
+
+            for (int d = 0; d < 3; d++) toppart += vectorA[d] * vectorB[d];
+
+            double vectorA2 = 0;
+            double vectorB2 = 0;
+
+            for (int d = 0; d <3;  d++)
+            {
+                vectorA2 += vectorA[d] * vectorA[d];
+                vectorB2 += vectorB[d] * vectorB[d];
+            }
+
+            double bottompart = 0;
+            bottompart = Math.Sqrt(vectorA2 * vectorB2);
+            
+            bool returndegree = false;
+            double rtnval = Math.Acos(toppart / bottompart);
+            if (returndegree) rtnval *= 360.0 / (2 * Math.PI);
+
+            Assert.IsTrue(returndegree);
+            
+
+            
+
+        }
+
+
+
+    }
+}
